8425c1571403d000070845a9c12e9333ecaa3138
Jindřich Bär <jindrichbar@gmail.com>
2023-04-16 11:49:24 +0200

    feat: svelte page (initial commit)

---
+++ website/src/routes/Offer.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let airport;
+    export let city;
+    export let price;
+    export let departureDate;
+    export let returnDate;
+</script>
+
+<div>
+    <h3>{city} ({airport}) - {price} USD</h3>
+    <p>{(new Date(departureDate)).toLocaleDateString()} - {(new Date(returnDate)).toLocaleDateString()}</p>
+</div>
---
+++ website/src/routes/Offers.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Offer from './Offer.svelte';
+	function sanitizeData(data) {
+		return data
+		.filter(x => x['Price (USD)'] !== 'undefined')
+		.map((item) => {
+			return {
+				city: item.City,
+				airport: item.Airport,
+				price: Number(item['Price (USD)']),
+				departureDate: item.From,
+				returnDate: item.To,
+			};
+		})
+		.sort((a,b) => a.price - b.price);
+	}
+
+	const url = "https://raw.githubusercontent.com/barjin/flight-tickets-scraper/master/results/recent.json";
+	const fetchData = (async () => {
+		const response = await fetch(url);
+    	const data = await response.json();
+
+		return sanitizeData(data);
+	})();
+</script>
+
+<pre>Last update at {(new Date().toLocaleDateString())}, 00:00</pre>
+{#await fetchData}
+	<p>Loading data...</p>
+{:then data}
+	{#each data.slice(0,5) as offer}
+		<Offer {...offer} />
+	{/each}
+{:catch error}
+	<p>An error occurred!</p>
+{/await}
