40c2ee1a9ae692a264f8390059ba9fc2acd62ebc
barjin <barjin@users.noreply.github.com>
2023-04-16 11:01:38 +0000

    Deploying to gh-pages from @ barjin/flight-tickets-scraper@9ec1a30bb20651b2ae68cfe2de3e8622562db6ac 🚀

---
+++ _app/immutable/chunks/1.85b348f5.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.9b64a2cc.js";export{t as component};
--- scripts/csvToJson.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cat $1 | python3 -c 'import csv, json, sys; print(json.dumps([dict(r) for r in csv.DictReader(sys.stdin)]))' | jq .
