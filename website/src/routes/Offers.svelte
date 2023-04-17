a060d0fee44915729d59780a683f12238d1b066c
Jindřich Bär <jindrichbar@gmail.com>
2023-04-17 22:33:57 +0200

    feat: update frontend app to match the current data schema

--- website/src/routes/Offers.svelte
+++ website/src/routes/Offers.svelte
@@ -2,14 +2,16 @@
 	import Offer from './Offer.svelte';
 	function sanitizeData(data) {
 		return data
-		.filter(x => x['Price (USD)'] && x['Price (USD)'] !== 'undefined')
+		.filter(x => x.flightprices.length > 0)
 		.map((item) => {
+			const bestOffer = item.flightprices.sort((a,b) => a.price - b.price)[0];
+			
 			return {
-				city: item.City,
-				airport: item.Airport,
-				price: Number(item['Price (USD)']),
-				departureDate: item.From,
-				returnDate: item.To,
+				city: `${item.e} ${item.label}`,
+				airport: item.iata,
+				price: bestOffer.price,
+				departureDate: bestOffer.dateFrom,
+				returnDate: bestOffer.dateTo,
 			};
 		})
 		.sort((a,b) => a.price - b.price);
@@ -28,7 +30,7 @@
 {#await fetchData}
 	<p>Loading data...</p>
 {:then data}
-	{#each data.slice(0,5) as offer}
+	{#each data.slice(0,10) as offer}
 		<Offer {...offer} />
 	{/each}
 {:catch error}
