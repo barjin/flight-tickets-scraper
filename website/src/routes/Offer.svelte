1fc7a1ca3e13782ebfaeae179e6e5a37c62a3d14
Jindřich Bär <jindrichbar@gmail.com>
2023-04-18 11:38:52 +0200

    feat: implement currency rate calculations (static)

--- website/src/routes/Offer.svelte
+++ website/src/routes/Offer.svelte
@@ -1,12 +1,26 @@
 <script>
+    import { getContext } from "svelte";
 	export let airport;
     export let city;
     export let price;
     export let departureDate;
     export let returnDate;
+    export let distance;
+
+    let rates;
+
+    const currencyStore = getContext('currencyRates');
+
+    currencyStore.subscribe(value => {
+        rates = value;
+    });
 </script>
 
-<div>
-    <h3>{city} ({airport}) - {price} USD</h3>
-    <p>{(new Date(departureDate)).toLocaleDateString()} - {(new Date(returnDate)).toLocaleDateString()}</p>
-</div>
+{#if rates}
+    <div>
+        <h3>{city} ({airport}) - {Math.ceil(price * rates['CZK'])} CZK</h3>
+        <p>{(new Date(departureDate)).toLocaleDateString()} - {(new Date(returnDate)).toLocaleDateString()}</p>
+    </div>
+{:else}
+    <p>Loading currency rates...</p>
+{/if}
--- website/src/routes/Offers.svelte
+++ website/src/routes/Offers.svelte
@@ -1,5 +1,8 @@
 <script>
 	import Offer from './Offer.svelte';
+	import { setContext } from 'svelte';
+	import { writable } from 'svelte/store';
+
 	function sanitizeData(data) {
 		return data
 		.filter(x => x.flightprices.length > 0)
@@ -12,17 +15,29 @@
 				price: bestOffer.price,
 				departureDate: bestOffer.dateFrom,
 				returnDate: bestOffer.dateTo,
+				distance: item.distance
 			};
 		})
 		.sort((a,b) => a.price - b.price);
 	}
 
 	const url = "https://raw.githubusercontent.com/barjin/flight-tickets-scraper/master/results/recent.json";
+	const currencyUrl = "https://raw.githubusercontent.com/barjin/flight-tickets-scraper/master/results/currency.json";
+
+	let currencyRates = writable(null);
+	setContext('currencyRates', currencyRates);
+
 	const fetchData = (async () => {
 		const response = await fetch(url);
     	const data = await response.json();
 
 		return sanitizeData(data);
+	})();
+	
+	(async () => {
+		const response = await fetch(currencyUrl);
+		const data = await response.json();
+		$currencyRates = data.rates;
 	})();
 </script>
 
